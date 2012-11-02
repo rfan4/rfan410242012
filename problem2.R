c65b48a542de3611db04ca099c0ec8a0439c50b8
Ron <rfan4@jhu.edu>
2012-11-02 11:15:26 -0400

    3rd commit

--- problem2.R
+++ problem2.R
@@ -4,3 +4,12 @@ names(winter)
 gold=winter[Medal=="Gold",]
 silver=winter[Medal=="Silver",]
 bronze=winter[Medal=="Bronze",]
+g=tapply(rep(1, dim(gold)[1]), gold$NOC, sum)
+s=tapply(rep(1, dim(silver)[1]), silver$NOC, sum)
+b=tapply(rep(1, dim(bronze)[1]), bronze$NOC, sum)
+name=names(g)
+dimnames(g)=NULL
+dimnames(s)=NULL
+dimnames(b)=NULL
+ranking=data.frame(NOC=name, Gold=g, Silver=s,Bronze=b)
+save(ranking, file = "problem2.RData", list = c('problem2'))
